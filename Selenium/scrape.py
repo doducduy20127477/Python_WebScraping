f98b3b8d5fbd4a825402ce63eb3d3c022452c6da
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2023-11-30 11:16:56 +0700

    feat: scrape data from table and export to csv file

--- Selenium/scrape.py
+++ Selenium/scrape.py
@@ -1,18 +1,51 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+import pandas as pd
 
 website = 'https://www.adamchoi.co.uk/overs/detailed'
-path = '/home/hcm-mki-l2077/Downloads'
+path = r'D:\RepoGithub\chromedriver-win64\chromedriver.exe'
 
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--no-sandbox')
-# chrome_options.headless = True
+try:
+	# service = Service(executable_path=r'/home/hcm-mki-l2077/Downloads')
+	service = Service(executable_path=path) # selenium 4
+	# options = Options()
+	# options.add_argument("--headless") # or use pyvirtualdiplay
+	# options.add_argument("--no-sandbox") # needed, because colab runs as root
 
-# service = Service(executable_path=r'/home/hcm-mki-l2077/Downloads')
+	# options.headless = True
+	driver = webdriver.Chrome(service=service) # selenium 4
 
-driver = webdriver.Chrome(path)
-driver.get(website)
+	# driver = webdriver.Chrome(path)
+	driver.get(website)
+
+	all_matches_button = driver.find_element(by="xpath", value='//label[@analytics-event="All matches"]')
+	all_matches_button.click()
+
+	matches = driver.find_elements(by="tag name", value='tr')
+	date = []
+	home_team = []
+	score = []
+	away_team = []
+	for match in matches:
+		# print(match.text)
+		# print(match.find_element(by="xpath", value='./td[1]').text)
+		date.append(match.find_element(by="xpath", value='./td[1]').text)
+		home = match.find_element(by="xpath", value='./td[2]').text
+		home_team.append(home)
+		# print(home)
+		score.append(match.find_element(by="xpath", value='./td[3]').text)
+		away_team.append(match.find_element(by="xpath", value='./td[4]').text)
+
+	df = pd.DataFrame({'date': date, 'home_team': home_team, 'score': score, 'away_team': away_team})
+	df.to_csv('football_data.csv', index=False)
+	print(df)
+
+except Exception as e:
+    print(f"An exception occurred: {str(e)}")
+finally:
+	# pass
+	# input("Press Enter to close the browser...")
+	driver.quit()
 
 
-driver.quit()
