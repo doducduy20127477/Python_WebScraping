1b288a05efdea963d2089e2baefae1a944cbe3e5
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2023-12-05 04:32:08 +0700

    feat: build a function scrape 1 page (no scrolling)

--- Selenium/Twitter/scrape.py
+++ Selenium/Twitter/scrape.py
@@ -9,9 +9,8 @@ import time
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
-website = 'https://twitter.com/i/flow/login'
+website = 'https://twitter.com/search?q=python&src=typed_query'
 path = r'D:\RepoGithub\chromedriver-win64\chromedriver.exe'
 
 options = Options()
@@ -22,8 +21,11 @@ driver = webdriver.Chrome(service=service, options=options)
 driver.get(website)
 driver.maximize_window()
 
+
 # LOAD USERNAME AND PASSWORD FROM .env
 # USE YOUR TWITTER ACCOUNT TO LOGIN
+load_dotenv()
+
 username = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//input[@autocomplete = "username"]')))
 username.send_keys(os.environ['TWITTER_USER'])
 
@@ -37,7 +39,30 @@ password.send_keys(os.environ['TWITTER_PASS'])
 login_button = driver.find_element(By.XPATH, '//div[@role="button"]//span[text()="Log in"]')
 login_button.click()
 
+def get_tweet(element):
+	# COMMENT: HANDLE EXCEPTION ELEMENT NOT FOUND
+	try:
+		user = element.find_element(By.XPATH, ".//span[contains(text(), '@')]").text
+		text = element.find_element(By.XPATH, ".//div[@lang]").text
+		tweet_data = [user, text]
+	except:
+		tweet_data = ['', '']
+	return tweet_data
 
+tweets = WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.XPATH, "//article[@role='article']")))
+
+user_data = []
+text_data = []
+for tweet in tweets:
+	tweet_data = get_tweet(tweet)
+	user_data.append(tweet_data[0])
+	text_data.append(" ".join(tweet_data[1].split()))
+
+
+df_tweets = pd.DataFrame({'user': user_data, 'text': text_data})
+df_tweets.to_csv('tweets.csv', index=False)
+print(df_tweets)
 
 input("Press Enter to close the browser...")
 driver.quit()
+
